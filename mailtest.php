6d063e5f236b7debeaebaa361a2a4115a53d613d
DMor <Dmor574@knights.ucf.edu>
2016-04-04 04:25:03 -0400

    Updated email test script

--- mailtest.php
+++ mailtest.php
@@ -1,10 +1,12 @@
 <?php
+         //$to = "biri@knights.ucf.edu";
          $to = "dmor574@knights.ucf.edu";
-         $subject = "COP4710 Automated Email - Test";
+		 $subject = "COP4710 Automated Email - Test";
          
          $message .= "<h1>Email Heading</h1>";
 		 $message .= "<p>This is an automated email send by visiting the webpage www.raspbiripi.ddns.net/mailtest.php</p>";
-         
+         $message .= "<p>Let me know if you got these emails</p>";
+
          $header = "From:abc@somedomain.com \r\n";
          $header .= "Cc:afgh@somedomain.com \r\n";
          $header .= "MIME-Version: 1.0\r\n";

77450d290ff107e28c33087122726008bc004b7b
DMor <Dmor574@knights.ucf.edu>
2016-04-04 04:21:13 -0400

    Configured Server for automated email. Uploaded test script

---
+++ mailtest.php
@@ -0,0 +1,20 @@
+<?php
+         $to = "dmor574@knights.ucf.edu";
+         $subject = "COP4710 Automated Email - Test";
+         
+         $message .= "<h1>Email Heading</h1>";
+		 $message .= "<p>This is an automated email send by visiting the webpage www.raspbiripi.ddns.net/mailtest.php</p>";
+         
+         $header = "From:abc@somedomain.com \r\n";
+         $header .= "Cc:afgh@somedomain.com \r\n";
+         $header .= "MIME-Version: 1.0\r\n";
+         $header .= "Content-type: text/html\r\n";
+         
+         $retval = mail ($to,$subject,$message,$header);
+         
+         if( $retval == true ) {
+            echo "Message sent successfully...";
+         }else {
+            echo "Message could not be sent...";
+         }
+?>

d5b7b34fd2027518f98945d4250f4257e3dcb9a7
DMor <Dmor574@knights.ucf.edu>
2016-04-07 15:56:28 -0400

    Commented and reformated db.php and login.php, deleted unused code

--- webpages/db.php
+++ webpages/db.php
@@ -1,48 +1,50 @@
 <?php
-session_start();
+	//Initialize Session
+	session_start();
 
-$servername = "raspbiripi.ddns.net";
-$servername = "localhost";
-$dbName = "cop4710db";
-$username = "pi";
-$password = "pipipi";
+	//Database Connection Information
+	$servername = "localhost"; 
+	$dbName = "cop4710db";
+	$username = "pi";
+	$password = "pipipi";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password);
+	//Create connection using specified credentials 
+	$conn = new mysqli($servername, $username, $password);
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
+	// Check connection for errors
+	if ($conn->connect_error) {
+		//Kill and exit script and print error message
+    	die("Connection failed: " . $conn->connect_error);
+	} 
 
-mysqli_select_db($conn, $dbName);
-//echo "Connected successfully";
+	//Select default database to query
+	mysqli_select_db($conn, $dbName);
 
-function debug_print($input)
-{
-	echo '<pre>';
-	echo var_dump($input);
-	echo '</pre>';
-}
-
-function get_semesterString($date)
-{
-	$year = substr($date,0,4);
-	$month = substr($date,5,2);
-	$semester = "";
-	if($month < 5)
+	//Print information about a variable
+	function debug_print($input)
 	{
-		$semester = "Spring ";
+		echo '<pre>';
+		echo var_dump($input);
+		echo '</pre>';
 	}
-	else if($month >= 5 && $month < 8)
+
+	//Given a date, return the semester it is part of (Fall, Spring, Summer)
+	function get_semesterString($date)
 	{
-		$semester = "Summer ";
+		//Strip out year and month
+		$year = substr($date,0,4);
+		$month = substr($date,5,2);
+		$semester = "";
+		
+		//Set Semester
+		if($month < 5)
+			$semester = "Spring ";
+		else if($month >= 5 && $month < 8)
+			$semester = "Summer ";
+		else
+			$semester = "Fall ";
+		
+		//Return semester and year
+		return $semester . $year;	
 	}
-	else
-	{
-		$semester = "Fall ";
-	}
-	return $semester . $year;
-	
-}
 ?>
--- webpages/login.php
+++ webpages/login.php
@@ -1,72 +1,75 @@
 <?php
-include_once("db.php");
+	//Import database connection and user session
+	include_once("db.php");
 
-if(isset($_COOKIE["user_id"]) && isset($_SESSION["user_id"]) && $_COOKIE["user_id"] == $_SESSION["user_id"])
-{
-	// the user is already logged in
-	// Trash the session?
-	//setcookie("user_id", $row["user_id"], -1, "/");
-	unset($_SESSION["user_id"]);
-	unset($_SESSION["role_id"]);
-	unset($_SESSION["name"]);
-	unset($_SESSION["email"]);
-}
-
-if(!empty($_POST))
-{
-	// check if pw is valid
-	$sql="
-	SELECT
-		users.user_id as user_id,
-		users.name as name,
-		users.email as email,
-		user_roles.role_id as role_id
-	FROM
-		users
-	INNER JOIN user_roles 
-	ON users.user_id = user_roles.user_id
-	WHERE
-		users.username='" . $_POST["loginEmail"] ."' 
-		AND users.password ='" . md5($_POST["loginPassword"]) . "'";
-
-	if ($result=mysqli_query($conn,$sql))
+	//Verify session and cookie match
+	if(isset($_COOKIE["user_id"]) && 
+	   isset($_SESSION["user_id"]) && 
+	   $_COOKIE["user_id"] == $_SESSION["user_id"])
 	{
-		// Fetch one and one row
-		/*
-		while ($row=mysqli_fetch_row($result))
-		{
-			printf ("%s (%s)\n",$row[0],$row[1]);
-		}
-		*/
-		// only need one row
-		
-		$row=mysqli_fetch_array($result);
-		
-		if($row["user_id"]!=null)
-		{
-			// store cookie
-			//setcookie("user_id", $row["user_id"], time() + (86400 * 30), "/");
-			$_SESSION["user_id"] = $row["user_id"];
-			$_SESSION["role_id"] = $row["role_id"];
-			$_SESSION["name"] = $row["name"];
-			$_SESSION["email"] = $row["email"];
-			echo "Sucessfully logged in.";
-		}
-		else
-		{
-			echo "Bad username or password.";
-		}
-		// Free result set
-		mysqli_free_result($result);
-		
+		//User already logged in - Trash the session?
+		//setcookie("user_id", $row["user_id"], -1, "/");
+		unset($_SESSION["user_id"]);
+		unset($_SESSION["role_id"]);
+		unset($_SESSION["name"]);
+		unset($_SESSION["email"]);
 	}
+	
+	//Execute if POST is NOT empty (Form Submitted)
+	if(!empty($_POST))
+	{
+		//SQL Query to check if username/password pair is valid and exists
+		$sql="
+		SELECT
+			users.user_id as user_id,
+			users.name as name,
+			users.email as email,
+			user_roles.role_id as role_id
+		FROM
+			users
+		INNER JOIN user_roles 
+		ON users.user_id = user_roles.user_id
+		WHERE
+			users.username='" . $_POST["loginEmail"] ."' 
+			AND users.password ='" . md5($_POST["loginPassword"]) . "'";
+		
+		//Execute query and store result.
+		//Continue if result exists
+		if ($result = mysqli_query($conn,$sql))
+		{
+			//Store first row of query results
+			$row = mysqli_fetch_array($result);
+		
+			//Continue if user_id exists in query results
+			if($row["user_id"] != null)
+			{
+				//Store session values
+				$_SESSION["user_id"] = $row["user_id"];
+				$_SESSION["role_id"] = $row["role_id"];
+				$_SESSION["name"] = $row["name"];
+				$_SESSION["email"] = $row["email"];
 
-	mysqli_close($conn);
+				//Display success message
+				echo "Sucessfully logged in.";
+			}
+			else
+			{
+				//Display Error - Unsuccessful login
+				echo "Bad username or password.";
+			}
+
+			//Free memory from query result
+			mysqli_free_result($result);
+		}
+
+		//Close connection to database
+		mysqli_close($conn);
 	
-	
-	die(); // don't render code below this point
-}
+		//If form submitted, don't render login page
+		die(); 
+	}
 ?>
+
 <html>
 	<head>
 		<title>Login</title>
@@ -98,15 +101,7 @@ if(!empty($_POST))
 							<input type="submit" class="buttons" value="Submit">
 						</td>
 					</tr>
-				</table>
-					
-					<!-- Email
-					<input type = "text" id = "loginEmail" name = "loginEmail"></br>
-
-					Password
-					<input type = "password" id = "loginPassword" name = "loginPassword"></br>
-
-					<input type="submit" value="Submit"> -->
+				</table>		
 			</form>
 		</div>
 	</body>

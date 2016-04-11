58e317b1b50a3d424fc48dbc889cadee6e1a8380
khanh.N <knguyen60@knights.ucf.edu>
2016-04-11 18:49:24 -0400

    changepassword.php added

--- webpages/changepassword.php
+++ webpages/changepassword.php
@@ -3,42 +3,65 @@
 	include_once("db.php"); //Connect to database and initialize session
 	
 	//if role is nominee, will kick out
-	//check_not_role(4);
+	check_not_role(4);
 	
-	$newpassword1=md5($_POST["newloginPassword"]);
-	$newpassword2=md5($_POST["newloginPasswordR"]);
 	
 	$passwordErr="";
 	if(!empty($_POST))
 	{
+		$oldpassword=md5($_POST["loginPassword"]);
+		$newpassword1=md5($_POST["newloginPassword"]);
+		$newpassword2=md5($_POST["newloginPasswordR"]);
+		//echo "$oldpassword<br>$newpassword1<br>$newpassword2<br>";
+		
+		//query to get password from db
+		
+		$sql="
+		SELECT password
+		FROM users
+		WHERE user_id='".$_SESSION["user_id"]."'
+		";
+		
+		$queryget=mysqli_query($conn,$sql);
+		//if($conn->query($sql)===TRUE){
+		$row=mysqli_fetch_assoc($queryget);
+		
+		$oldpassworddb= $row['password'];
+		
+		
 		//check if the typed in login password is right
-		
-		
+
+		if($oldpassword==$oldpassworddb){
+			
 		//check if the 2 new passwords match
-		if($newpassword1 == $newpassword2){
-		
+			if($newpassword1 == $newpassword2){
 			
-			//update the the newpassword
-			$sql="
-			UPDATE users
-			SET password=$newpassword1
-			WHERE user_id='".$_SESSION["user_id"]."'
-			";
-			
-			if($conn->query($sql)===TRUE){
-				$conn->close();
-				session_destroy();
-				die("Password has been changed <br><a href='login.php'>Return</a> the the login page");
+				
+				//update the the newpassword
+				$sql="
+				UPDATE users
+				SET password='$newpassword1'
+				WHERE user_id='".$_SESSION["user_id"]."'
+				";
+				
+				if($conn->query($sql)===TRUE){
+					$conn->close();
+					session_destroy();
+					die("Password has been changed <br><a href='login.php'>Return</a> the the login page");
+				}
+				
+				else{
+					echo "Error: " . $sql . "<br>" . $conn->error;
+
+				}
 			}
 			
 			else{
-				echo "Error: " . $sql . "<br>" . $conn->error;
-
+				die("New passwords don't match<br><a href='changepassword.php'>Return</a>");
 			}
 		}
-		
 		else{
-			die("New passwords don't match");
+			die("Old password doesn't match<br><a href='changepassword.php'>Return</a>");
 		}
 		
 		
@@ -66,11 +89,11 @@
 
 			<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
 				<table>
-					<!--<tr>
+					<tr>
 						<td>Current password:</td>
 						<td>&emsp;</td>
-						<td><input type = "password" id = "loginPassword" name = "loginPassword" /><span class="error"><</span></td>
-					</tr-->
+						<td><input type = "text" id = "loginPassword" name = "loginPassword" /></td>
+					</tr>
 
 					<tr>
 						<td>New Password:</td>
--- webpages/login_check.php
+++ webpages/login_check.php
@@ -23,4 +23,28 @@
 			header('Location: login.php'); // kick the user to the login page
 		}	
 	}
+	
+	function check_not_role($role_id)
+	{	
+		//Flag Variable
+		$isvalid = true;
+	
+		//If userid is not set, role is not valid		
+		if(!isset($_SESSION["user_id"]))
+			$isvalid = false;
+	
+		//If roleid is set and roleid stored in session does not match input, not valid
+		if(isset($_SESSION["role_id"]) && $_SESSION["role_id"] == $role_id)
+			$isvalid = false;
+	
+		//Set values if role is valid
+		if(!$isvalid)
+		{
+			// the user IS NOT already logged in
+			// Trash the session and force to login page
+			setcookie("user_id", $row["user_id"], -1, "/");
+			unset($_SESSION["user_id"]);
+			header('Location: login.php'); // kick the user to the login page
+		}	
+	}
 ?>

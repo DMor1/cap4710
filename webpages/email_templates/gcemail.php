6a5abf4f8c8b8eb612c7b54f99d00a734da7bcd7
DMor <Dmor574@knights.ucf.edu>
2016-04-12 17:37:38 -0400

    Reformatted GCEmail.php

--- webpages/email_templates/gcemail.php
+++ webpages/email_templates/gcemail.php
@@ -1,27 +1,28 @@
 <?php
 	function getGCEmailBody($name, $role, $user, $pass) {
 		$msg = '<html>
-	<head>
-		<title>You have been selected for the Graduate Committee</title>
-	</head>
-	<body>
-		Hello ' . $name . ',
-		</br></br>
-		The System Administrator for this session has chosen you as ' . $role . '
-		</br></br>
-		Your login is: ' .  $user . '
-		</br></br>
-		Your password is: ' . $pass . '
-		</br></br>
-		In order to access the scoring table please click on the following url: <a href="gcmembers.php">Click Here</a>
-		</br></br>
-	</body>
-	<footer>
-		<hr>
-		GTAMS Administration
-	</footer>
-</html>';
+					<head>
+						<title>You have been selected for the Graduate Committee</title>
+					</head>			
+					
+					<body>
+						<p>Hello ' . $name . ',</p>
+		
+						<p>The System Administrator for this session has chosen you as ' . $role . '</p>
 
+						<p>
+							<p>Login: ' .  $user . '</p>
+							<p>Password: ' . $pass . '</p>
+						</p>
+
+						<p>In order to access the scoring table please click on the following url: <a href="gcmembers.php">Click Here</a></p>
+					</body>
+					
+					<footer>
+						<hr>
+							GTAMS Administration
+					</footer>
+				</html>';
 
 		return $msg;
 	}

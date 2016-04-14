2eec3ce3b7f05ac732c4658aca401664aa9a7107
Bobtastic <newmark.robert@knights.ucf.edu>
2016-04-14 08:39:10 -0400

    new clean db

--- sql/cop4710db.sql
+++ sql/cop4710db.sql
@@ -1,70 +1,38 @@
--- phpMyAdmin SQL Dump
--- version 4.6.0
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Apr 14, 2016 at 03:59 AM
--- Server version: 5.5.44-0+deb8u1
--- PHP Version: 5.6.19-0+deb8u1
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `cop4710db`
---
+CREATE DATABASE IF NOT EXISTS `cop4710db` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `cop4710db`;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `advisors`
---
-
-CREATE TABLE `advisors` (
+CREATE TABLE IF NOT EXISTS `advisors` (
   `user_id` int(11) NOT NULL,
   `advisor_name` varchar(255) NOT NULL,
   `start_year` int(4) NOT NULL,
-  `end_year` int(4) NOT NULL
+  `end_year` int(4) NOT NULL,
+  PRIMARY KEY (`user_id`,`advisor_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `courses`
---
-
-CREATE TABLE `courses` (
-  `course_id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `courses` (
+  `course_id` int(11) NOT NULL AUTO_INCREMENT,
   `course_name` varchar(255) NOT NULL,
   `course_desc` int(11) NOT NULL,
   `course_grade` varchar(255) NOT NULL,
-  `course_credit_hours` int(11) NOT NULL
+  `course_credit_hours` int(11) NOT NULL,
+  PRIMARY KEY (`course_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `courses_taken`
---
-
-CREATE TABLE `courses_taken` (
+CREATE TABLE IF NOT EXISTS `courses_taken` (
   `course_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`course_id`,`user_id`),
+  KEY `courses_taken_fk2` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `nominees`
---
-
-CREATE TABLE `nominees` (
+CREATE TABLE IF NOT EXISTS `nominees` (
   `session_id` int(11) NOT NULL,
   `nominee_user_id` int(11) NOT NULL,
   `nominated_by_user_id` int(11) NOT NULL,
@@ -79,35 +47,26 @@ CREATE TABLE `nominees` (
   `phd_advisor_name` varchar(255) NOT NULL,
   `receiveNomination` date DEFAULT NULL,
   `respondNomination` date DEFAULT NULL,
-  `verifiedNomination` date DEFAULT NULL
+  `verifiedNomination` date DEFAULT NULL,
+  PRIMARY KEY (`session_id`,`nominee_user_id`),
+  KEY `nominated_by_user_id` (`nominated_by_user_id`),
+  KEY `speak_test_id` (`speak_test_id`),
+  KEY `nominees_fk2` (`nominee_user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `publications`
---
-
-CREATE TABLE `publications` (
+CREATE TABLE IF NOT EXISTS `publications` (
   `session_id` int(11) NOT NULL,
   `nominee_user_id` int(11) NOT NULL,
-  `publication_name_and_citations` text NOT NULL
+  `publication_name_and_citations` text NOT NULL,
+  PRIMARY KEY (`session_id`,`nominee_user_id`),
+  KEY `publications_fk2` (`nominee_user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `roles`
---
-
-CREATE TABLE `roles` (
-  `role_id` int(11) NOT NULL,
-  `description` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `roles`
---
+CREATE TABLE IF NOT EXISTS `roles` (
+  `role_id` int(11) NOT NULL AUTO_INCREMENT,
+  `description` varchar(255) NOT NULL,
+  PRIMARY KEY (`role_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 INSERT INTO `roles` (`role_id`, `description`) VALUES
 (1, 'System Administrator'),
@@ -115,251 +74,88 @@ INSERT INTO `roles` (`role_id`, `description`) VALUES
 (3, 'Nominator'),
 (4, 'Nominee');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `scores`
---
-
-CREATE TABLE `scores` (
+CREATE TABLE IF NOT EXISTS `scores` (
   `session_id` int(11) NOT NULL,
   `nominee_user_id` int(11) NOT NULL,
   `gc_user_id` int(11) NOT NULL,
-  `score` double NOT NULL
+  `score` double NOT NULL DEFAULT '0',
+  `comment` varchar(255) NOT NULL,
+  PRIMARY KEY (`session_id`,`nominee_user_id`,`gc_user_id`),
+  KEY `scores_fk2` (`nominee_user_id`),
+  KEY `scores_fk3` (`gc_user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `sessions`
---
-
-CREATE TABLE `sessions` (
-  `session_id` int(11) NOT NULL,
-  `start_date` date NOT NULL,
-  `end_date` date NOT NULL,
-  `initiation_date` date NOT NULL,
-  `verify_deadline_date` date NOT NULL
+CREATE TABLE IF NOT EXISTS `sessions` (
+  `session_id` int(11) NOT NULL AUTO_INCREMENT,
+  `session_name` varchar(255) NOT NULL,
+  `start_date` datetime NOT NULL,
+  `end_date` datetime NOT NULL,
+  `initiation_date` datetime NOT NULL,
+  `verify_deadline_date` datetime NOT NULL,
+  PRIMARY KEY (`session_id`,`session_name`) USING BTREE,
+  UNIQUE KEY `session_name` (`session_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `speak_test`
---
-
-CREATE TABLE `speak_test` (
-  `speak_test_id` int(11) NOT NULL,
-  `status` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `speak_test`
---
+CREATE TABLE IF NOT EXISTS `speak_test` (
+  `speak_test_id` int(11) NOT NULL AUTO_INCREMENT,
+  `status` varchar(255) NOT NULL,
+  PRIMARY KEY (`speak_test_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 INSERT INTO `speak_test` (`speak_test_id`, `status`) VALUES
 (1, 'Yes'),
 (2, 'No'),
 (3, 'Graduated from a U.S. institution');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
-  `user_id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `fname` varchar(255) NOT NULL,
+  `lname` varchar(255) NOT NULL,
   `phonenumber` varchar(255) NOT NULL,
   `pid` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `username` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=217 DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `users`
---
+INSERT INTO `users` (`user_id`, `fname`, `lname`, `phonenumber`, `pid`, `email`, `username`, `password`) VALUES
+(1, 'System Admin', '', '', '', 'admin@test.com', 'admin', '098f6bcd4621d373cade4e832627b4f6');
 
-INSERT INTO `users` (`user_id`, `name`, `phonenumber`, `pid`, `email`, `username`, `password`) VALUES
-(1, 'System Admin', '', '', 'admin@test.com', 'admin', '098f6bcd4621d373cade4e832627b4f6');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user_roles`
---
-
-CREATE TABLE `user_roles` (
-  `role_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `user_roles`
---
+CREATE TABLE IF NOT EXISTS `user_roles` (
+  `role_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`role_id`,`user_id`),
+  KEY `user_roles_fk2` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 INSERT INTO `user_roles` (`role_id`, `user_id`) VALUES
 (1, 1);
 
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `advisors`
---
-ALTER TABLE `advisors`
-  ADD PRIMARY KEY (`user_id`,`advisor_name`);
-
---
--- Indexes for table `courses`
---
-ALTER TABLE `courses`
-  ADD PRIMARY KEY (`course_id`);
-
---
--- Indexes for table `courses_taken`
---
-ALTER TABLE `courses_taken`
-  ADD PRIMARY KEY (`course_id`,`user_id`),
-  ADD KEY `courses_taken_fk2` (`user_id`);
-
---
--- Indexes for table `nominees`
---
-ALTER TABLE `nominees`
-  ADD PRIMARY KEY (`session_id`,`nominee_user_id`),
-  ADD KEY `nominated_by_user_id` (`nominated_by_user_id`),
-  ADD KEY `speak_test_id` (`speak_test_id`),
-  ADD KEY `nominees_fk2` (`nominee_user_id`);
-
---
--- Indexes for table `publications`
---
-ALTER TABLE `publications`
-  ADD PRIMARY KEY (`session_id`,`nominee_user_id`),
-  ADD KEY `publications_fk2` (`nominee_user_id`);
-
---
--- Indexes for table `roles`
---
-ALTER TABLE `roles`
-  ADD PRIMARY KEY (`role_id`);
-
---
--- Indexes for table `scores`
---
-ALTER TABLE `scores`
-  ADD PRIMARY KEY (`session_id`,`nominee_user_id`,`gc_user_id`),
-  ADD KEY `scores_fk2` (`nominee_user_id`),
-  ADD KEY `scores_fk3` (`gc_user_id`);
-
---
--- Indexes for table `sessions`
---
-ALTER TABLE `sessions`
-  ADD PRIMARY KEY (`session_id`);
-
---
--- Indexes for table `speak_test`
---
-ALTER TABLE `speak_test`
-  ADD PRIMARY KEY (`speak_test_id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`user_id`),
-  ADD UNIQUE KEY `username` (`username`);
-
---
--- Indexes for table `user_roles`
---
-ALTER TABLE `user_roles`
-  ADD PRIMARY KEY (`role_id`,`user_id`),
-  ADD KEY `user_roles_fk2` (`user_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `courses`
---
-ALTER TABLE `courses`
-  MODIFY `course_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT for table `roles`
---
-ALTER TABLE `roles`
-  MODIFY `role_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
---
--- AUTO_INCREMENT for table `sessions`
---
-ALTER TABLE `sessions`
-  MODIFY `session_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT for table `speak_test`
---
-ALTER TABLE `speak_test`
-  MODIFY `speak_test_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=144;
---
--- AUTO_INCREMENT for table `user_roles`
---
-ALTER TABLE `user_roles`
-  MODIFY `role_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `advisors`
---
 ALTER TABLE `advisors`
   ADD CONSTRAINT `advisors_fk1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
 
---
--- Constraints for table `courses_taken`
---
 ALTER TABLE `courses_taken`
   ADD CONSTRAINT `courses_taken_fk1` FOREIGN KEY (`course_id`) REFERENCES `courses` (`course_id`) ON DELETE CASCADE,
   ADD CONSTRAINT `courses_taken_fk2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
 
---
--- Constraints for table `nominees`
---
 ALTER TABLE `nominees`
   ADD CONSTRAINT `nominees_fk1` FOREIGN KEY (`session_id`) REFERENCES `sessions` (`session_id`),
   ADD CONSTRAINT `nominees_fk2` FOREIGN KEY (`nominee_user_id`) REFERENCES `users` (`user_id`),
   ADD CONSTRAINT `nominees_fk3` FOREIGN KEY (`nominated_by_user_id`) REFERENCES `users` (`user_id`),
   ADD CONSTRAINT `nominees_fk4` FOREIGN KEY (`speak_test_id`) REFERENCES `speak_test` (`speak_test_id`);
 
---
--- Constraints for table `publications`
---
 ALTER TABLE `publications`
   ADD CONSTRAINT `publications_fk1` FOREIGN KEY (`session_id`) REFERENCES `sessions` (`session_id`),
   ADD CONSTRAINT `publications_fk2` FOREIGN KEY (`nominee_user_id`) REFERENCES `users` (`user_id`);
 
---
--- Constraints for table `scores`
---
 ALTER TABLE `scores`
   ADD CONSTRAINT `scores_fk1` FOREIGN KEY (`session_id`) REFERENCES `sessions` (`session_id`),
   ADD CONSTRAINT `scores_fk2` FOREIGN KEY (`nominee_user_id`) REFERENCES `users` (`user_id`),
   ADD CONSTRAINT `scores_fk3` FOREIGN KEY (`gc_user_id`) REFERENCES `users` (`user_id`);
 
---
--- Constraints for table `user_roles`
---
 ALTER TABLE `user_roles`
   ADD CONSTRAINT `user_roles_fk1` FOREIGN KEY (`role_id`) REFERENCES `roles` (`role_id`),
   ADD CONSTRAINT `user_roles_fk2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE;
